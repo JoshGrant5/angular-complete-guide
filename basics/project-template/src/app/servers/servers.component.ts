ef2c775ed765aa1ba88c1b8f229addd0c761a07b
JoshGrant5 <joshgg@icloud.com>
2020-12-23 13:47:47 -0800

    *ngIf is a structural directive that responds to a boolean

--- basics/project-template/src/app/servers/servers.component.ts
+++ basics/project-template/src/app/servers/servers.component.ts
@@ -10,6 +10,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
   serverName: any = 'Test Server';
+  serverCreated = false;
 
   constructor() {
     setTimeout(() => {
@@ -22,6 +23,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStatus = 'Server was created with name of ' + this.serverName;
+    this.serverCreated = true;
   }
 
   onUpdateServerName(event: Event) {
