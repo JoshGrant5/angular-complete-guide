918b97a0d917850b5a43ff639b8dc2168491d5a1
JoshGrant5 <joshgg@icloud.com>
2020-12-23 13:37:08 -0800

    Replace the event binding with 2-way binding

--- basics/project-template/src/app/servers/servers.component.ts
+++ basics/project-template/src/app/servers/servers.component.ts
@@ -9,7 +9,7 @@ export class ServersComponent implements OnInit {
 
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
-  serverName: any = '';
+  serverName: any = 'Test Server';
 
   constructor() {
     setTimeout(() => {
@@ -21,7 +21,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created';
+    this.serverCreationStatus = 'Server was created with name of ' + this.serverName;
   }
 
   onUpdateServerName(event: Event) {
